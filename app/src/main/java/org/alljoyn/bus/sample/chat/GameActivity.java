a5bce61dfb265bc7103e51d31ff606044887b4a0
haukesand <haukesa@web.de>
2016-07-14 11:02:07 +0200

    Game colors

--- app/src/main/java/org/alljoyn/bus/sample/chat/GameActivity.java
+++ app/src/main/java/org/alljoyn/bus/sample/chat/GameActivity.java
@@ -16,23 +16,55 @@ import android.widget.TextView;
 public class GameActivity extends Activity{
 
     private static final String TAG = "GameActivity";
-
+    RelativeLayout rl;
+    ImageView hourGlass, chosen, notChosen;
+    TextView text;
     public void onCreate(Bundle savedInstanceState) {
         Log.i(TAG, "Game started");
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.question);
 
-        RelativeLayout rl = (RelativeLayout)findViewById(R.id.my_rl);
-        rl.setBackgroundColor(Color.BLUE);
+        rl = (RelativeLayout)findViewById(R.id.my_rl);
+        text = (TextView)findViewById(R.id.textView);
+        hourGlass = (ImageView)findViewById(R.id.imageView3);
+        notChosen = (ImageView)findViewById(R.id.imageView);
+        chosen = (ImageView)findViewById(R.id.imageView2);
 
-        ImageView hourGlass = (ImageView)findViewById(R.id.imageView3);
+    setChosen("THis is your Question?!!");
+    }
+
+    public void setWaitOthers(){
+        rl.setBackgroundColor(Color.rgb(210,250,245));
+        notChosen.setVisibility(View.INVISIBLE);
+        chosen.setVisibility(View.INVISIBLE);
         hourGlass.setVisibility(View.VISIBLE);
 
-        TextView text = (TextView)findViewById(R.id.textView);
         text.setText("Wait for other Players");
 
+    }
 
+    public void setChosen(String Question){
+        rl.setBackgroundColor(Color.rgb(244,150,150));
+
+        notChosen.setVisibility(View.INVISIBLE);
+        chosen.setVisibility(View.VISIBLE);
+        hourGlass.setVisibility(View.INVISIBLE);
+
+        TextView text = (TextView)findViewById(R.id.textView);
+        text.setText("Answer: " +Question);
+
+    }
+
+    public void setNotChosen(String Question){
+        rl.setBackgroundColor(Color.rgb(210,250,245));
+
+        notChosen.setVisibility(View.VISIBLE);
+        chosen.setVisibility(View.INVISIBLE);
+        hourGlass.setVisibility(View.INVISIBLE);
+
+        TextView text = (TextView)findViewById(R.id.textView);
+        text.setText(Question);
 
     }
 }
