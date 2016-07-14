05527e4842e417c31d0e2f161b6ba161f679cb04
NaNa <nathalie.dittrich@stud.uni-due.de>
2016-07-15 00:41:33 +0200

    added game logic

--- app/src/main/java/org/alljoyn/bus/sample/chat/GameActivity.java
+++ app/src/main/java/org/alljoyn/bus/sample/chat/GameActivity.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Nana on 13.07.16.
@@ -26,23 +27,128 @@ public class GameActivity extends Activity {
     RelativeLayout rl;
     ImageView hourGlass, chosen, notChosen;
     TextView text;
+    String [][] users = new String[3][2];
+    private int x = 0;
+    private String myName;
+    private int myId;
+    private int winner;
+    private boolean meSet = false;
+
+    //todo:
+    //create twodimensional array with 3 elements
+    //get remote usernames (when '1' posted -> get order for user index)
+    //create variable "me"
+    //send remote usernames
+    //compare with other users usernames
+    //username not in own list = 'me' -> add 'me' to userlist
+    //user with index 0 does math.random -> posts result
+    //resulting index = winner
 
     private Runnable running = new Runnable() {
         @Override
         public void run() {
-            history = mChatApplication.getHistory();
-            for (String s : history) {
-                if (s.equals(String.valueOf('1'))) {
-                    readyCount++;
-                    if (readyCount == 3) {
-                        //start game
-                        Log.d(TAG, "all members ready");
-                        break;
+            while (true){
+                try{
+                    Thread.sleep(100);
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                history = mChatApplication.getHistory();
+                for (String s : history) {
+                    Log.d(TAG, s);
+                    if (s.substring(s.length()-1, s.length()).equals(String.valueOf('1'))) {
+                        //get remote nicknames
+                        if(s.charAt(11) == ')') {
+                            String temp = s.substring(1,10);
+                            boolean isInList = false;
+                            for(int i = 0; i<users.length; i++){
+                                if(users[i][1].equals(temp)){
+                                    isInList = true;
+                                }
+                            }
+                            if(!isInList) {
+                                users[x][1] = temp;
+                                users[x][2] = String.valueOf(x);
+                                readyCount++;
+                                x++;
+                            }
+
+                        }
+                        else{
+                            if(s.substring(1,3).equals("Me")){
+                                if(!meSet) {
+                                    x++;
+                                    meSet = true;
+                                }
+                            }
+                        }
+                        if (readyCount == 2) {
+                            for(int i = 0; i<users.length; i++){
+                                mChatApplication.newLocalUserMessage("l: " + users[i][1]);
+                            }
+                        }
+                    } else {
+                        if (s.substring(12,13).equals("l")) {
+                            boolean inList = false;
+                            for(int i = 0; i<users.length; i++){
+                                if(s.equals(users[i][1])==false){
+                                    inList = true;
+                                }
+                            }
+                            if(!inList){
+                                users[2][1] = s.substring(16, s.length());
+                                myName = s.substring(16, s.length());
+                                if(Integer.parseInt(users[0][2]) == 0){
+                                    if(Integer.parseInt(users[1][2]) == 1){
+                                        users[2][2] = "2";
+                                        myId = 2;
+                                    }
+                                    else{
+                                        if(Integer.parseInt(users[0][2]) == 2){
+                                            users[2][2] = "1";
+                                            myId = 1;
+                                        }
+                                    }
+                                }
+                                else{
+                                    if(Integer.parseInt(users[0][2]) == 1){
+                                        if(Integer.parseInt(users[1][2]) == 0){
+                                            users[2][2] = "2";
+                                            myId = 2;
+                                        }
+                                        if(Integer.parseInt(users[1][2]) == 2){
+                                            users[2][2] = "0";
+                                            myId = 0;
+                                        }
+                                    }
+                                    else{
+                                        if(Integer.parseInt(users[1][2]) == 0){
+                                            users[2][2] = "1";
+                                            myId = 1;
+                                        }
+                                        if(Integer.parseInt(users[1][2]) == 1){
+                                            users[2][2] = "0";
+                                            myId = 1;
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
-                } else {
-                    if (s.equals(String.valueOf('0'))) {
-                        //todo
-                        break;
+                    for(int i = 0; i<users.length; i++){
+                        Log.d(TAG, "username: " + users[i][1]);
+                        Log.d(TAG, "userId: " + users[i][2]);
+                    }
+                    if(myId == 0) {
+                        winner = new Random().nextInt(3);
+                        mChatApplication.newLocalUserMessage("w: " + winner);
+                        if (winner == myId){
+                            //I got chosen!!!
+                        }
+                    }
+                    if(s.substring(12,13).equals("w") && s.substring(s.length()-1, s.length()).equals(String.valueOf(myId))){
+                        //you got chosen!!!
                     }
                 }
             }
